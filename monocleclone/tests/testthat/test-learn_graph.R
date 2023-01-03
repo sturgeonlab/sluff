47d8f4945345e512c27fbaa74b18b55a7b33652f
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2023-01-03 15:43:51 -0500

    Add files via upload

---
+++ monocleclone/tests/testthat/test-learn_graph.R
@@ -0,0 +1,122 @@
+context("test-learn_graph")
+skip_not_travis <- function ()
+{
+  if (identical(Sys.getenv("TRAVIS"), "true")) {
+    return(invisible(TRUE))
+  }
+  skip("Not on Travis")
+}
+
+cds <- load_a549()
+
+test_that("test learn_graph error messages work", {
+  skip_on_travis()
+  expect_error(cds <- learn_graph(cds),
+               "No dimensionality reduction for UMAP calculated. Please run reduce_dimension with reduction_method = UMAP and cluster_cells before running learn_graph.")
+  cds <- preprocess_cds(cds)
+  expect_error(cds <- learn_graph(cds),
+               "No dimensionality reduction for UMAP calculated. Please run reduce_dimension with reduction_method = UMAP and cluster_cells before running learn_graph.")
+  cds <- reduce_dimension(cds)
+  expect_error(cds <- learn_graph(cds),
+               "No cell clusters for UMAP calculated. Please run cluster_cells with reduction_method = UMAP before running learn_graph.")
+  #expect_error(cds <- learn_graph(cds, learn_graph_control = list(FALSE)), "")
+  expect_error(cds <- learn_graph(cds, learn_graph_control = list(prune = FALSE)), "Unknown variable in learn_graph_control")
+})
+
+set.seed(42)
+cds <- preprocess_cds(cds)
+cds <- reduce_dimension(cds)
+cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1)
+cds <- cluster_cells(cds, resolution = .01)
+
+test_that("learn_graph stays the same", {
+  skip_on_travis()
+  cds <- learn_graph(cds)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 63)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "3")
+
+  # Force partition
+  temp <- rep(c(1,2), length.out=length(partitions(cds)))
+  names(temp) <- names(partitions(cds))
+  cds@clusters[["UMAP"]]$partitions <- temp
+  cds <- learn_graph(cds, use_partition = FALSE)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 63)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "3")
+
+  cds <- learn_graph(cds)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 104)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "24")
+
+  cds <- learn_graph(cds, close_loop = TRUE)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 104)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "24")
+
+  cds <- learn_graph(cds, learn_graph_control = list(prune_graph = FALSE))
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 216)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "46")
+})
+
+
+#### TRAVIS ####
+
+
+cds <- load_a549()
+
+test_that("test learn_graph error messages work", {
+  skip_not_travis()
+  expect_error(cds <- learn_graph(cds),
+               "No dimensionality reduction for UMAP calculated. Please run reduce_dimension with reduction_method = UMAP and cluster_cells before running learn_graph.")
+  cds <- preprocess_cds(cds)
+  expect_error(cds <- learn_graph(cds),
+               "No dimensionality reduction for UMAP calculated. Please run reduce_dimension with reduction_method = UMAP and cluster_cells before running learn_graph.")
+  cds <- reduce_dimension(cds)
+  expect_error(cds <- learn_graph(cds),
+               "No cell clusters for UMAP calculated. Please run cluster_cells with reduction_method = UMAP before running learn_graph.")
+  #expect_error(cds <- learn_graph(cds, learn_graph_control = list(FALSE)), "")
+  expect_error(cds <- learn_graph(cds, learn_graph_control = list(prune = FALSE)), "Unknown variable in learn_graph_control")
+})
+
+set.seed(42)
+cds <- preprocess_cds(cds)
+cds <- reduce_dimension(cds)
+cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1)
+cds <- cluster_cells(cds, resolution = .01)
+
+test_that("learn_graph stays the same", {
+  skip_not_travis()
+  cds <- learn_graph(cds)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 33)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "24")
+
+  # Force partition
+  temp <- rep(c(1,2), length.out=length(partitions(cds)))
+  names(temp) <- names(partitions(cds))
+  cds@clusters[["UMAP"]]$partitions <- temp
+  cds <- learn_graph(cds, use_partition = FALSE)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 33)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "24")
+
+  cds <- learn_graph(cds)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 74)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "36")
+
+  cds <- learn_graph(cds, close_loop = TRUE)
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 74)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "36")
+
+  cds <- learn_graph(cds, learn_graph_control = list(prune_graph = FALSE))
+  expect_is(principal_graph(cds)[["UMAP"]], "igraph")
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 144)
+  expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "8")
+})
+
+
---
+++ monocleclone/tests/testthat/test-reduce_dimension.R
@@ -0,0 +1,200 @@
+context("test-reduce_dimension")
+skip_not_travis <- function ()
+{
+  if (identical(Sys.getenv("TRAVIS"), "true")) {
+    return(invisible(TRUE))
+  }
+  skip("Not on Travis")
+}
+set.seed(100)
+cds <- load_a549()
+cds <- estimate_size_factors(cds)
+
+
+test_that('Nearest neighbors', {
+  cds <- preprocess_cds(cds)
+  cds <- reduce_dimension(cds, build_nn_index=TRUE)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['nn_index']][['annoy']][['nn_index']][['metric']], 'euclidean')
+})
+
+
+test_that("reduce_dimension runs", {
+  skip_on_travis()
+  expect_error(cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1),
+               "Data has not been preprocessed with chosen method: PCA Please run preprocess_cds with method = PCA before running reduce_dimension.")
+  cds <- preprocess_cds(cds, num_dim = 20)
+
+  cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1)
+  expect_equal(nrow(reducedDims(cds)$UMAP), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$UMAP), 2)
+  expect_equal(as.numeric(reducedDims(cds)$UMAP[1,1]), -2.86,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds, max_components = 3, umap.fast_sgd=FALSE, cores=1, reduction_method = "UMAP")
+  expect_equal(nrow(reducedDims(cds)$UMAP), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$UMAP), 3)
+  expect_equal(as.numeric(reducedDims(cds)$UMAP[1,1]), 1.69,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds, reduction_method = "tSNE")
+  expect_equal(nrow(reducedDims(cds)$tSNE), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$tSNE), 2)
+  expect_equal(as.numeric(reducedDims(cds)$tSNE[1,1]), -1.16,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds,  max_components = 3, reduction_method = "tSNE")
+  expect_equal(nrow(reducedDims(cds)$tSNE), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$tSNE), 3)
+  expect_equal(as.numeric(reducedDims(cds)$tSNE[1,1]), -4.29,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds, reduction_method = "PCA")
+  expect_equal(nrow(reducedDims(cds)$PCA), nrow(colData(cds)))
+  expect_equal(as.numeric(reducedDims(cds)$PCA["E11_A01_RT_467","PC1"]),
+               2.4207391, tolerance = 1e-4)
+
+  cds <- preprocess_cds(cds, num_dim = 20, method = "LSI")
+  cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1, preprocess_method = "LSI")
+  expect_equal(nrow(reducedDims(cds)$UMAP), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$UMAP), 2)
+  expect_equal(as.numeric(reducedDims(cds)$UMAP[1,1]), -0.163,
+               tolerance = 1e-3)
+
+  cds <- reduce_dimension(cds, reduction_method = "tSNE", preprocess_method = "LSI")
+  expect_equal(nrow(reducedDims(cds)$tSNE), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$tSNE), 2)
+  expect_equal(as.numeric(reducedDims(cds)$tSNE[1,1]), -2.15,
+               tolerance = 1e-2)
+
+  # check model
+  set.seed(100)
+  cds <- load_a549()
+  cds <- estimate_size_factors(cds)
+  cds <- preprocess_cds(cds, num_dim = 20)
+  cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_preprocess_method']], 'PCA')
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['max_components']], 2, tol=1e1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_metric']], 'cosine')
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_min_dist']], 0.1, tol=1e-1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_n_neighbors']], 15, tol=1e1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_fast_sgd']], FALSE)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_model']][['embedding']][[1,1]], -1.80, tol=1e-1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_model']][['n_neighbors']][[1]], 15, tol=1e1)
+
+
+  expect_error(reduce_dimension(cds, reduction_method = "DDRTree"),
+               "reduction_method must be one of 'UMAP', 'PCA', 'tSNE', 'LSI', 'Aligned'")
+})
+
+test_that("reduce_dimension clears old graphs", {
+  skip_on_travis()
+  cds <- preprocess_cds(cds)
+  cds <- reduce_dimension(cds)
+  cds <- cluster_cells(cds)
+  cds <- learn_graph(cds)
+  cds <- reduce_dimension(cds)
+  expect_null(cds@principal_graph_aux[["UMAP"]])
+  expect_error(partitions(cds), "No partitions calculated for reduction_method = UMAP. Please first run cluster_cells with reduction_method = UMAP.")
+  expect_error(clusters(cds), "No clusters calculated for reduction_method = UMAP. Please first run cluster_cells with reduction_method = UMAP.")
+  expect_null(cds@principal_graph[["UMAP"]])
+
+  cds <- cluster_cells(cds)
+  cds <- learn_graph(cds)
+  cds <- reduce_dimension(cds, reduction_method = "tSNE")
+  expect_is(clusters(cds), "factor")
+  testthat::expect_is(cds@principal_graph[["UMAP"]], "igraph")
+})
+
+
+#### TRAVIS ####
+
+
+set.seed(100)
+cds <- load_a549()
+cds <- estimate_size_factors(cds)
+
+test_that("reduce_dimension runs", {
+  skip_not_travis()
+  expect_error(cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1),
+               "Data has not been preprocessed with chosen method: PCA Please run preprocess_cds with method = PCA before running reduce_dimension.")
+  cds <- preprocess_cds(cds, num_dim = 20)
+
+  cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1)
+  expect_equal(nrow(reducedDims(cds)$UMAP), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$UMAP), 2)
+  expect_equal(as.numeric(reducedDims(cds)$UMAP[1,1]), 2.26,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds, max_components = 3, umap.fast_sgd=FALSE, cores=1, reduction_method = "UMAP")
+  expect_equal(nrow(reducedDims(cds)$UMAP), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$UMAP), 3)
+  expect_equal(as.numeric(reducedDims(cds)$UMAP[1,1]), 0.265,
+               tolerance = 1e-3)
+
+  cds <- reduce_dimension(cds, reduction_method = "tSNE")
+  expect_equal(nrow(reducedDims(cds)$tSNE), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$tSNE), 2)
+  expect_equal(as.numeric(reducedDims(cds)$tSNE[1,1]), -3.07,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds,  max_components = 3, reduction_method = "tSNE")
+  expect_equal(nrow(reducedDims(cds)$tSNE), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$tSNE), 3)
+  expect_equal(as.numeric(reducedDims(cds)$tSNE[1,1]),  4.58,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds, reduction_method = "PCA")
+  expect_equal(nrow(reducedDims(cds)$PCA), nrow(colData(cds)))
+  expect_equal(as.numeric(reducedDims(cds)$PCA["E11_A01_RT_467","PC1"]),
+               2.4207391, tolerance = 1e-4)
+
+  cds <- preprocess_cds(cds, num_dim = 20, method = "LSI")
+  cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1, preprocess_method = "LSI")
+  expect_equal(nrow(reducedDims(cds)$UMAP), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$UMAP), 2)
+  expect_equal(as.numeric(reducedDims(cds)$UMAP[1,1]), -1.21,
+               tolerance = 1e-2)
+
+  cds <- reduce_dimension(cds, reduction_method = "tSNE", preprocess_method = "LSI")
+  expect_equal(nrow(reducedDims(cds)$tSNE), nrow(colData(cds)))
+  expect_equal(ncol(reducedDims(cds)$tSNE), 2)
+  expect_equal(as.numeric(reducedDims(cds)$tSNE[1,1]), -2.97,
+               tolerance = 1e-2)
+
+  # check model
+  set.seed(100)
+  cds <- load_a549()
+  cds <- estimate_size_factors(cds)
+  cds <- preprocess_cds(cds, num_dim = 20)
+  cds <- reduce_dimension(cds, umap.fast_sgd=FALSE, cores=1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_preprocess_method']], 'PCA')
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['max_components']], 2, tol=1e1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_metric']], 'cosine')
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_min_dist']], 0.1, tol=1e-1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_n_neighbors']], 15, tol=1e1)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_fast_sgd']], FALSE)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_model']][['embedding']][[1,1]], 2.03, tol=1e-2)
+  expect_equal(cds@reduce_dim_aux[['UMAP']][['model']][['umap_model']][['n_neighbors']][[1]], 15, tol=1e1)
+
+  expect_error(reduce_dimension(cds, reduction_method = "DDRTree"),
+               "reduction_method must be one of 'UMAP', 'PCA', 'tSNE', 'LSI', 'Aligned'")
+})
+
+test_that("reduce_dimension clears old graphs", {
+  skip_not_travis()
+  cds <- preprocess_cds(cds)
+  cds <- reduce_dimension(cds)
+  cds <- cluster_cells(cds)
+  cds <- learn_graph(cds)
+  cds <- reduce_dimension(cds)
+  expect_null(cds@principal_graph_aux[["UMAP"]])
+  expect_error(partitions(cds), "No partitions calculated for reduction_method = UMAP. Please first run cluster_cells with reduction_method = UMAP.")
+  expect_error(clusters(cds), "No clusters calculated for reduction_method = UMAP. Please first run cluster_cells with reduction_method = UMAP.")
+  expect_null(cds@principal_graph[["UMAP"]])
+
+  cds <- cluster_cells(cds)
+  cds <- learn_graph(cds)
+  cds <- reduce_dimension(cds, reduction_method = "tSNE")
+  expect_is(clusters(cds), "factor")
+  testthat::expect_is(cds@principal_graph[["UMAP"]], "igraph")
+})
